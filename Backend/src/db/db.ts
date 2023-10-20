74ec937fc3637995ed09320d7c2fc223a0c80504
Desmond Tong <tysdesmond2@gmail.com>
2023-10-21 01:19:51 +0800

    add database connection file

---
+++ Backend/src/db/db.ts
@@ -0,0 +1,17 @@
+import mysql from "mysql2";
+
+const pool = mysql.createPool({
+  host: process.env.HOST,
+  user: process.env.USER,
+  database: process.env.DATABASE,
+  password: process.env.PASSWORD,
+  waitForConnections: true,
+  connectionLimit: 10,
+  maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
+  idleTimeout: 60000, // idle connections timeout, in milliseconds, the default value 60000
+  queueLimit: 0,
+  enableKeepAlive: true,
+  keepAliveInitialDelay: 0,
+});
+
+export default pool;
