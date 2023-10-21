f03ff57725df7b01dcef039878523ccaea1cadea
Desmond Tong <tysdesmond2@gmail.com>
2023-10-22 02:05:16 +0800

    Add authorization and validator for project router

--- Backend/src/routers/projects.ts
+++ Backend/src/routers/projects.ts
@@ -1,6 +1,13 @@
 import express from "express";
 const router = express.Router();
 
+import { auth, authCustomer, authSupplier } from "../middleware/auth";
+import {
+  validateIdInParam,
+  validateCreateProject,
+  validateUpdateProject,
+} from "../validators/projects";
+import { validation as checkValid } from "../middleware/checkValid";
 import {
   createProject,
   getAllCustomerProjects,
@@ -11,13 +18,50 @@ import {
   deleteItem,
 } from "../controllers/projects";
 
-router.put("/projects-items", createProject);
-router.get("/projects-items/:customer_id", getAllCustomerProjects);
-router.get("/projects-items", getAllProjects);
-router.post("/projects-items/:project_id", getProjectById);
-router.delete("/projects-items/:project_id", deleteProject);
-router.patch("/projects-items/:project_id", updateProject);
+router.put(
+  "/projects-items",
+  authCustomer,
+  validateCreateProject,
+  checkValid,
+  createProject
+);
+router.get(
+  "/projects-items/:customer_id",
+  authCustomer,
+  validateIdInParam,
+  checkValid,
+  getAllCustomerProjects
+);
+router.get("/projects-items", authSupplier, getAllProjects);
+router.post(
+  "/projects-items/:project_id",
+  auth,
+  validateIdInParam,
+  checkValid,
+  getProjectById
+);
+router.delete(
+  "/projects-items/:project_id",
+  authCustomer,
+  validateIdInParam,
+  checkValid,
+  deleteProject
+);
+router.patch(
+  "/projects-items/:project_id",
+  authCustomer,
+  validateIdInParam,
+  validateUpdateProject,
+  checkValid,
+  updateProject
+);
 
-router.delete("/items/:item_id", deleteItem);
+router.delete(
+  "/items/:item_id",
+  authCustomer,
+  validateIdInParam,
+  checkValid,
+  deleteItem
+);
 
 export default router;
---
+++ Backend/src/validators/projects.ts
@@ -0,0 +1,62 @@
+import { body, check, param } from "express-validator";
+
+const validateIdInParam = [
+  param("project_id", "invalid id").optional().isLength({ min: 1, max: 10 }),
+  param("item_id", "invalid id").optional().isLength({ min: 1, max: 10 }),
+  param("customer_id", "invalid id").optional().isLength({ min: 1, max: 10 }),
+];
+
+const validateCreateProject = [
+  body("customer_id", "customer id is required")
+    .notEmpty()
+    .isLength({ min: 1, max: 10 }),
+  body("project_name", "project name is required").notEmpty().isString(),
+  check("items", "items must be an array").notEmpty().isArray(),
+  check("items.*.technology", "technology is required").notEmpty().isString(),
+  check("items.*.material", "material is required").notEmpty().isString(),
+  check("items.*.surface_finish", "surface finish is required")
+    .notEmpty()
+    .isString(),
+  check("items.*.item_name", "item name is required").notEmpty().isString(),
+  check("items.*.quantity", "quantity is required").notEmpty().isInt(),
+];
+
+const validateUpdateProject = [
+  body("project_name", "project name must be string")
+    .optional()
+    .notEmpty()
+    .isString(),
+  body("is_active", "is_active must be a boolean")
+    .optional()
+    .notEmpty()
+    .isBoolean(),
+
+  check("items", "items must be an array").optional().isArray(),
+  check("items.*.item_id", "item ID must be integer").optional().notEmpty().isInt(),
+  check("items.*.technology", "technology must be string")
+    .optional()
+    .notEmpty()
+    .isString(),
+  check("items.*.material", "material must be string")
+    .optional()
+    .notEmpty()
+    .isString(),
+  check("items.*.surface_finish", "surface finish must be string")
+    .optional()
+    .notEmpty()
+    .isString(),
+  check("items.*.item_name", "item name must be string")
+    .optional()
+    .notEmpty()
+    .isString(),
+  check("items.*.quantity", "quantity must be integer")
+    .optional()
+    .notEmpty()
+    .isInt(),
+  check("items.*.status", "status must be string")
+    .optional()
+    .notEmpty()
+    .isString(),
+];
+
+export { validateIdInParam, validateCreateProject, validateUpdateProject };
