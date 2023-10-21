8248ca9be849cc6e002ba3c870d5b2d66ed95b44
Desmond Tong <tysdesmond2@gmail.com>
2023-10-22 01:37:11 +0800

    Add authorization to constraints routers

--- Backend/src/routers/constraint.ts
+++ Backend/src/routers/constraint.ts
@@ -1,6 +1,7 @@
 import express from "express";
 const router = express.Router();
 
+import { auth } from "../middleware/auth";
 import {
   getRoles,
   getItemStatuses,
@@ -10,13 +11,13 @@ import {
   getSurfaceFinishes,
 } from "../controllers/constraint";
 
-router.get("/constraints/roles", getRoles);
+router.get("/constraints/roles", auth, getRoles);
 
-router.get("/constraints/item-statuses", getItemStatuses);
-router.get("/constraints/qt-statuses", getQtStatuses);
+router.get("/constraints/item-statuses", auth, getItemStatuses);
+router.get("/constraints/qt-statuses", auth, getQtStatuses);
 
-router.get("/constraints/technologies", getTechnologies);
-router.get("/constraints/materials", getMaterials);
-router.get("/constraints/surface-finishes", getSurfaceFinishes);
+router.get("/constraints/technologies", auth, getTechnologies);
+router.get("/constraints/materials", auth, getMaterials);
+router.get("/constraints/surface-finishes", auth, getSurfaceFinishes);
 
 export default router;
