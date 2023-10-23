9e3f5898347f432a9c36b2c91f1622906a6aed6b
Desmond Tong <tysdesmond2@gmail.com>
2023-10-23 11:08:37 +0800

    Add creationmodal

--- Frontend/src/components/CreationModal.tsx
+++ Frontend/src/components/CreationModal.tsx
@@ -5,11 +5,12 @@ import {
   DialogContent,
   DialogContentText,
   DialogTitle,
+  MenuItem,
   Stack,
   TextField,
   Typography,
 } from "@mui/material";
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect, useRef, useState } from "react";
 import useFetch from "../hooks/useFetch";
 import { FetchedData, Props, data } from "../interfaces";
 import UserContext from "../context/user";
@@ -20,6 +21,15 @@ const CreationModal: React.FC<Props> = (props) => {
   const fetchData = useFetch();
   const userCtx = useContext(UserContext);
   const navigate = useNavigate();
+
+  const [technologies, setTechnologies] = useState<string[]>([]);
+  const [materials, setMaterials] = useState<string[]>([]);
+  const [surfaceFinishes, setSurfaceFinishes] = useState<string[]>([]);
+
+  const projectNameRef = useRef<HTMLInputElement>();
+  const technologyRef = useRef<HTMLInputElement>();
+  const materialRef = useRef<HTMLInputElement>();
+  const surfaceFinishRef = useRef<HTMLInputElement>();
 
   // endpoint
   const createProject = async () => {
@@ -39,8 +49,58 @@ const CreationModal: React.FC<Props> = (props) => {
     }
   };
 
+  const getTechnologies = async () => {
+    const res: data = await fetchData(
+      "/api/constraints/technologies",
+      "GET",
+      undefined,
+      userCtx?.accessToken
+    );
+
+    if (res.ok) {
+      setTechnologies(res.data.msg);
+    } else {
+      alert(JSON.stringify(res.data));
+    }
+  };
+
+  const getMaterials = async () => {
+    const res: data = await fetchData(
+      "/api/constraints/materials",
+      "GET",
+      undefined,
+      userCtx?.accessToken
+    );
+
+    if (res.ok) {
+      setMaterials(res.data.msg);
+    } else {
+      alert(JSON.stringify(res.data));
+    }
+  };
+
+  const getSurfaceFinishes = async () => {
+    const res: data = await fetchData(
+      "/api/constraints/surface-finishes",
+      "GET",
+      undefined,
+      userCtx?.accessToken
+    );
+
+    if (res.ok) {
+      setSurfaceFinishes(res.data.msg);
+    } else {
+      alert(JSON.stringify(res.data));
+    }
+  };
+
   useEffect(() => {
+    // new project
     userCtx?.claims.role == "CUSTOMER" && createProject();
+
+    getTechnologies();
+    getMaterials();
+    getSurfaceFinishes();
   }, []);
   return (
     <>
@@ -49,31 +109,78 @@ const CreationModal: React.FC<Props> = (props) => {
         onClose={() => props.setOpenCreateModal?.(false)}
         scroll="body"
       >
-        <DialogTitle sx={{ p: 0 }} className="pic-display">
-            New Project
+        <DialogTitle sx={{ m: "1rem" }} variant="h5">
+          New Project
         </DialogTitle>
-        <DialogContent sx={{ m: "1rem" }}>
-          <DialogContentText
-            my="1rem"
-            variant="h5"
-            color="var(--darkgrey-text)"
-          >
-            New Project
-            
-            <TextField
-              autoFocus
-              required
-              margin="normal"
-              id="description"
-              label="Description"
-              multiline
-              rows={2}
-              type="text"
-              fullWidth
-            //   inputRef={descriptionRef}
-            />
 
-          </DialogContentText>
+        <DialogContent sx={{ m: "1rem" }}>
+          <TextField
+            autoFocus
+            required
+            margin="normal"
+            id="name"
+            label="Project Name"
+            multiline
+            rows={2}
+            type="text"
+            fullWidth
+            inputRef={projectNameRef}
+          />
+
+          {/* item details */}
+          <TextField
+            select
+            required
+            margin="normal"
+            id="technology"
+            label="Tecnology"
+            type="text"
+            fullWidth
+            defaultValue=""
+            inputRef={technologyRef}
+          >
+            {technologies.map((item, idx) => (
+              <MenuItem key={idx} value={item}>
+                {item}
+              </MenuItem>
+            ))}
+          </TextField>
+
+          <TextField
+            select
+            required
+            margin="normal"
+            id="material"
+            label="Material"
+            type="text"
+            fullWidth
+            defaultValue=""
+            inputRef={materialRef}
+          >
+            {materials.map((item, idx) => (
+              <MenuItem key={idx} value={item}>
+                {item}
+              </MenuItem>
+            ))}
+          </TextField>
+
+          <TextField
+            select
+            required
+            margin="normal"
+            id="surfaceFinish"
+            label="Surface Finish"
+            type="text"
+            fullWidth
+            defaultValue=""
+            inputRef={surfaceFinishRef}
+          >
+            {surfaceFinishes.map((item, idx) => (
+              <MenuItem key={idx} value={item}>
+                {item}
+              </MenuItem>
+            ))}
+          </TextField>
         </DialogContent>
 
         <DialogActions></DialogActions>
